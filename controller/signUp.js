7b780de95524169ed34524d61bffcd98fc3a8910
chamakurichandu <chandra.ch@abitechnologies.in>
2021-03-18 18:54:09 +0530

    done

---
+++ controller/signUp.js
@@ -0,0 +1,43 @@
+const mongoose=require('mongoose');
+const signUpTemplateCopy=new mongoose.Schema({
+    fullName:{
+        type:String,
+        unique:true,
+        trim:true,
+        min:8,
+        max:15,
+        require:true
+    },
+    firstName:{
+        type:String,
+        unique:true,
+        trim:true,
+        min:3,
+        require:true
+    },
+    lastName:{
+        type:String,
+        unique:true,
+        trim:true,
+        min:3,
+        max:3,
+        require:true
+    },
+    email:{
+        type:String,
+        unique:true,
+        trim:true,
+        require:true
+    },
+    password:{
+        type:String,
+        unique:true,
+        trim:true,
+        min:5,
+        max:15,
+        require:true
+    },
+
+})
+
+module.exports=mongoose.model('mytable',signUpTemplateCopy)
--- server.js
+++ server.js
@@ -1,19 +1,61 @@
 const express=require('express');
 const app=express();
+const mongoose=require('mongoose');
+const cors=require('cors');
+
+//importing router
+
+const routerUrl=require('');
+//connecting to mongodb
+
+ mongoose.connect('mongodb://localhost:27017/test' ,{useNewUrlParser: true, useUnifiedTopology: true},()=>{
+            console.log('database connected')
+ })
+
+app.use(express.json());
+app.use(cors());
+app.use('/api',routerUrl)
+
+
 const PORT=2000
-
-app.get('/',(req,res)=>{
-    res.send('welcome to backend')
-})
-app.get('/data',(req,res)=>{
-    const id=req.query.id
-    res.send('welcome to the node js' +id)
-})
-app.get('/data/:id',(req,res)=>{
-    const id=req.params.id
-    res.send('welcome to the node js' +id)
-})
-
 app.listen(PORT,()=>{
-    console.log(`server listening at port no ${PORT}`)
+    console.log(`Server listening at port no${PORT}`)
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const express=require('express');
+// const app = express();
+// const cors=require('cors');
+// const mongoose=require('mongoose');
+
+
+// //import from router
+// const routerUrls=require('./routes/user')
+
+// //connect to the mongoose
+// mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true, useUnifiedTopology: true},()=>{
+//     console.log('database connected')
+// })
+// app.use(cors())
+// app.use(express.json());
+
+// //using router
+// app.use('/app',routerUrls)
+
+// app.listen(2000,()=>{
+//     console.log('server running on port no :2000')
+// })
