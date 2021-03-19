d49a21acfcd147521e4c47b36e9a45a206d6af0b
chamakurichandu <chandra.ch@abitechnologies.in>
2021-03-19 11:43:26 +0530

    done

--- routers/router.js
+++ routers/router.js
@@ -1,13 +1,22 @@
 
+const { response } = require('express');
 const express=require('express');
 const router=express.Router()
 const signUpTemplateCopy = require('../controller/signUp');
 
+     const index=  (req,res)=>{
+        signUpTemplateCopy.find()
+        .then(response=>{
+            res.json({response})
+        })
+        .catch(error=>{
+            res.json({
+                message:"an error occured"
+            })
+        })
+     }
 
-
-router.get('/',(req,res)=>{
-            res.send('welcome to nodejs')
-})
+router.get('/',index)
 router.post('/signup',(req,res)=>{
     const signUpUser=new signUpTemplateCopy({
         fullName:req.body.fullName,
